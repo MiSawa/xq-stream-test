2c912a2fc6ac4291849a2ac5c95b56cb3eb0bed6
mi_sawa <mi.sawa.1216+git@gmail.com>
2022-03-28 19:58:36 +0900

    Implement threaded one with GAT

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use anyhow::Result;
-use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
+#![feature(generic_associated_types)]
+
+use std::{
+    cell::RefCell,
+    marker::PhantomData,
+    sync::mpsc::{sync_channel, SyncSender},
+};
+
+use anyhow::{anyhow, Result};
 
 #[derive(Clone, Debug)]
 enum Index {
@@ -34,7 +41,7 @@ impl Value {
             }
         }
         print!("]");
-        if let Some(value) = self.value {
+        if let Some(value) = &self.value {
             print!(",");
             match value {
                 PrimitiveValue::Null => print!("null"),
@@ -44,42 +51,32 @@ impl Value {
             }
         }
         print!("]");
-        println!("");
+        println!();
     }
 }
 
 struct StreamState<'a> {
-    sender: SyncSender<Value>,
+    sender: SyncSender<Result<Value>>,
     path: &'a mut Path,
 }
 
 impl<'a> StreamState<'a> {
-    fn emit_path(&self) {
-        print!("[");
-        for (i, v) in self.path.iter().enumerate() {
-            if i != 0 {
-                print!(",");
-            }
-            match v {
-                Index::Array(i) => print!("{i}"),
-                Index::Map(s) => print!("{s:?}"),
-            }
-        }
-        print!("]");
-    }
-
     fn emit_value(&mut self, value: PrimitiveValue) {
-        self.sender.send(Value {
-            path: self.path.clone(),
-            value: Some(value),
-        });
+        self.sender
+            .send(Ok(Value {
+                path: self.path.clone(),
+                value: Some(value),
+            }))
+            .ok(); // Discarding err since this indicates that the recever has already been dropped so they should already know what to do.
     }
 
     fn emit_close(&self) {
-        self.sender.send(Value {
-            path: self.path.clone(),
-            value: None,
-        });
+        self.sender
+            .send(Ok(Value {
+                path: self.path.clone(),
+                value: None,
+            }))
+            .ok(); // Discarding err since this indicates that the recever has already been dropped so they should already know what to do.
     }
 }
 
@@ -161,7 +158,7 @@ impl<'de, 'a> serde::de::Visitor<'de> for &mut StreamState<'a> {
     {
         let mut i = 0;
         self.path.push(Index::Array(i));
-        while let Some(_) = seq.next_element_seed(&mut *self)? {
+        while seq.next_element_seed(&mut *self)?.is_some() {
             self.path.pop();
             i += 1;
             self.path.push(Index::Array(i));
@@ -236,28 +233,115 @@ impl<'de, 'a> serde::de::DeserializeSeed<'de> for &mut StreamState<'a> {
     }
 }
 
-struct Stream;
-impl<'de> serde::Deserialize<'de> for Stream {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let mut path = vec![];
-        let (sender, recv) = sync_channel(1);
-        let mut visitor = StreamState {
-            sender,
-            path: &mut path,
-        };
-        deserializer.deserialize_any(&mut visitor)?;
-        Ok(Self)
+trait MultiDocDeserializer<'de> {
+    type Error: std::error::Error;
+    type Iterator<T: serde::Deserialize<'de>>: Iterator<Item = Result<T, Self::Error>>;
+    fn into_multidoc_iter<T: serde::Deserialize<'de>>(self) -> Self::Iterator<T>;
+}
+
+impl<'de, R: serde_json::de::Read<'de>> MultiDocDeserializer<'de>
+    for serde_json::de::Deserializer<R>
+{
+    type Error = serde_json::Error;
+    type Iterator<T: serde::Deserialize<'de>> = serde_json::de::StreamDeserializer<'de, R, T>;
+
+    fn into_multidoc_iter<T: serde::Deserialize<'de>>(self) -> Self::Iterator<T> {
+        self.into_iter()
     }
 }
 
+struct SerdeYamlMultiDocIter<'de, T> {
+    inner: serde_yaml::Deserializer<'de>,
+    _phantom: PhantomData<T>,
+}
+impl<'de, T: serde::Deserialize<'de>> Iterator for SerdeYamlMultiDocIter<'de, T> {
+    type Item = Result<T, serde_yaml::Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().map(|de| T::deserialize(de))
+    }
+}
+impl<'de> MultiDocDeserializer<'de> for serde_yaml::Deserializer<'de> {
+    type Error = serde_yaml::Error;
+    type Iterator<T: serde::Deserialize<'de>> = SerdeYamlMultiDocIter<'de, T>;
+
+    fn into_multidoc_iter<T: serde::Deserialize<'de>>(self) -> Self::Iterator<T> {
+        SerdeYamlMultiDocIter {
+            inner: self,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+trait FromReader {
+    type De<'de, R>: MultiDocDeserializer<'de>
+    where
+        R: 'de + std::io::Read;
+    fn from_reader<'de, R: 'de + std::io::Read>(read: R) -> Self::De<'de, R>;
+}
+struct Yaml;
+impl FromReader for Yaml {
+    type De<'de, R> = serde_yaml::Deserializer<'de> where R: 'de + std::io::Read;
+
+    fn from_reader<'de, R: 'de + std::io::Read>(read: R) -> Self::De<'de, R> {
+        serde_yaml::Deserializer::from_reader(read)
+    }
+}
+struct Json;
+impl FromReader for Json {
+    type De<'de, R> = serde_json::Deserializer<serde_json::de::IoRead<R>> where R: 'de + std::io::Read;
+
+    fn from_reader<'de, R: 'de + std::io::Read>(read: R) -> Self::De<'de, R> {
+        serde_json::Deserializer::from_reader(read)
+    }
+}
+
+fn main_generic<T: FromReader>() -> impl Iterator<Item = Result<Value>> {
+    let (sender, receiver) = sync_channel(1);
+    std::thread::spawn(|| {
+        thread_local! {
+            static SENDER: RefCell<Option<SyncSender<Result<Value>>>> = RefCell::new(None);
+        }
+        SENDER.with(|snd| snd.borrow_mut().replace(sender));
+        struct Stream;
+        impl<'de> serde::Deserialize<'de> for Stream {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                let mut path = vec![];
+                let sender = SENDER.with(|snd| snd.borrow().as_ref().unwrap().clone());
+                let mut visitor = StreamState {
+                    sender,
+                    path: &mut path,
+                };
+                deserializer.deserialize_any(&mut visitor)?;
+                Ok(Self)
+            }
+        }
+        let de = T::from_reader(std::io::stdin().lock());
+        for v in de.into_multidoc_iter::<Stream>() {
+            if let Err(e) = v {
+                SENDER
+                    .with(|snd| {
+                        snd.borrow_mut()
+                            .as_ref()
+                            .unwrap()
+                            .send(Err(anyhow!("Deserialization error: {e}")))
+                    })
+                    .ok();
+                break;
+            }
+        }
+    });
+    receiver.into_iter()
+}
+
 fn main() -> Result<()> {
-    let stdin = std::io::stdin();
-    for v in serde_json::de::Deserializer::from_reader(stdin).into_iter::<Stream>() {
-        if let Err(e) = v {
-            eprintln!("{:?}", e);
+    for v in main_generic::<Json>() {
+        match v {
+            Ok(v) => v.print(),
+            Err(e) => eprintln!("{e}"),
         }
     }
     Ok(())
